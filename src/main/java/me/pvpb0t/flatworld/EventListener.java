6d09cb1dbcbc8d795f2935abf4745ad6395a4f86
pvpb0t <74259011+pvpb0t@users.noreply.github.com>
2023-01-26 19:32:26 +0100

    fixed

--- src/main/java/me/pvpb0t/flatworld/EventListener.java
+++ src/main/java/me/pvpb0t/flatworld/EventListener.java
@@ -1,9 +1,14 @@
 package me.pvpb0t.flatworld;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.VillagerAcquireTradeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantRecipe;
 
@@ -18,11 +23,26 @@ public class EventListener implements Listener {
     }
 
     @EventHandler
-    public void onVillagerTrade(VillagerAcquireTradeEvent event) {
-        MerchantRecipe trade = event.getRecipe();
-        ItemStack result = trade.getResult();
+    public void onPlayerMakeTrade(final InventoryClickEvent event) {
+        if (event.getInventory().getType() != InventoryType.MERCHANT) return;
+        if (!(event.getInventory().getHolder() instanceof Villager)) return;
+        if (!(event.getWhoClicked() instanceof Player)) return;
+        if (event.getRawSlot() != 2) return;
+        final Player player = (Player) event.getWhoClicked();
+        final Villager villager = (Villager) event.getInventory().getHolder();
+
+
+        //Get the items involved in the trade
+        final ItemStack result = event.getCurrentItem();
+        ItemStack ingredient1 = event.getInventory().getItem(0);
+        ItemStack ingredient2 = event.getInventory().getItem(1);
+        if (result == null || result.getType() == Material.AIR) return;
+        if (ingredient1 == null) ingredient1 = new ItemStack(Material.AIR, 1);
+        if (ingredient2 == null) ingredient2 = new ItemStack(Material.AIR, 1);
+
         if (result.getType() == Material.EMERALD) {
             int amount = result.getAmount();
+
             //Check if we should use 3x emeralds
             if (Math.random() * 100 < threeXChance) {
                 amount *= 3;
@@ -31,7 +51,16 @@ public class EventListener implements Listener {
             if (Math.random() * 100 < fiveXChance) {
                 amount *= 5;
             }
-            event.getEntity().getInventory().addItem(new ItemStack(Material.EMERALD, amount));
+
+
+            player.getInventory().addItem(new ItemStack(Material.EMERALD, amount));
         }
+
     }
-}
+
+
+
+
+    }
+
+
