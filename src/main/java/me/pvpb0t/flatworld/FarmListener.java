2cd6724b8ce96dff40ab234a8e765ac81cec19dd
pvpb0t <74259011+pvpb0t@users.noreply.github.com>
2023-01-27 19:41:53 +0100

    hope no1 abuses

--- src/main/java/me/pvpb0t/flatworld/FarmListener.java
+++ src/main/java/me/pvpb0t/flatworld/FarmListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -30,49 +31,70 @@ public class FarmListener implements Listener {
     @EventHandler
     public void onClick(PlayerInteractEvent e) {
         if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-        Block block = e.getClickedBlock();
+            Block block = e.getClickedBlock();
             assert block != null;
             log.info(block.toString());
-        Material blockType = block.getType();
-        log.info(blockType.name());
+            Material blockType = block.getType();
+            log.info(blockType.name());
+            final Player player = e.getPlayer();
 
-        if(blockType == Material.CARROTS || blockType == Material.WHEAT ||blockType == Material.BEETROOT|| blockType == Material.POTATOES) {
-            ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
-            Material itemType = item.getType();
-            log.info(itemType.name());
+            if(blockType == Material.CARROTS || blockType == Material.WHEAT || blockType == Material.BEETROOTS || blockType == Material.POTATOES) {
+                ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
+                Material itemType = item.getType();
+                log.info(itemType.name());
 
-            if (itemType == Material.CARROT || itemType == Material.BEETROOT_SEEDS || itemType == Material.WHEAT_SEEDS || itemType == Material.POTATO) {
-                BlockData blockData = block.getBlockData();
-                // check if the crop is fully grown
-                if (blockData instanceof Ageable && ((Ageable) blockData).getAge() == ((Ageable) blockData).getMaximumAge()) {
+                // check that the player is holding a valid seed/crop
+                if (itemType == Material.CARROT || itemType == Material.BEETROOT_SEEDS || itemType == Material.WHEAT_SEEDS || itemType == Material.POTATO) {
+                    BlockData blockData = block.getBlockData();
+                    // check that the crop is fully grown
+                    if (blockData instanceof Ageable && ((Ageable) blockData).getAge() == ((Ageable) blockData).getMaximumAge()) {
 
-                    log.info(blockData.getAsString());
-                    // set new data for the block
-                    if (itemType == Material.CARROT) {
-                        block.setType(Material.CARROTS);
-                        block.setBlockData(Material.CARROTS.createBlockData());
-                    } else if (itemType == Material.BEETROOT_SEEDS) {
-                        block.setType(Material.BEETROOT_SEEDS);
-                        block.setBlockData(Material.BEETROOTS.createBlockData());
-                    } else if (itemType == Material.WHEAT_SEEDS) {
-                        block.setType(Material.WHEAT_SEEDS);
-                        block.setBlockData(Material.WHEAT.createBlockData());
-                    } else if (itemType == Material.POTATO) {
-                        block.setType(Material.POTATO);
-                        block.setBlockData(Material.POTATOES.createBlockData());
+                        log.info(blockData.getAsString());
+                        // check that the seed/crop being held matches the block being clicked
+                        if (blockType == Material.CARROTS && itemType == Material.CARROT ||
+                                blockType == Material.BEETROOTS && itemType == Material.BEETROOT_SEEDS ||
+                                blockType == Material.WHEAT && itemType == Material.WHEAT_SEEDS ||
+                                blockType == Material.POTATOES && itemType == Material.POTATO) {
+
+                            // add the farmed crop to the player's inventory
+                            e.getPlayer().getInventory().addItem(new ItemStack(blockType));
+                            // remove the seed/crop from the player's inventory
+                            int amount = e.getPlayer().getInventory().getItemInMainHand().getAmount();
+                            if (amount > 1) {
+                                e.getPlayer().getInventory().getItemInMainHand().setAmount(amount - 1);
+                            } else {
+                                e.getPlayer().getInventory().setItemInMainHand(null);
+                            }
+                            // replant the crop
+                            if (itemType == Material.CARROT) {
+                                block.setType(Material.CARROTS);
+                                block.setBlockData(Material.CARROTS.createBlockData());
+                            } else if (itemType == Material.BEETROOT_SEEDS) {
+                                block.setType(Material.BEETROOTS);
+                                block.setBlockData(Material.BEETROOTS.createBlockData());
+                            } else if (itemType == Material.WHEAT_SEEDS) {
+                                block.setType(Material.WHEAT);
+                                block.setBlockData(Material.WHEAT.createBlockData());
+                            }else if (itemType == Material.POTATO) {
+                                block.setType(Material.POTATOES);
+                                block.setBlockData(Material.POTATOES.createBlockData());
+                            }
+                            //e.getPlayer().getInventory().addItem(new ItemStack(blockType,3));
+                            player.getInventory().addItem(new ItemStack(itemType, 3));
+
+                        }else {
+//if the player is not holding the correct item then dont plant the new crop and dont drop any items
+                            e.getPlayer().sendMessage("You need to hold the correct seed to replant the crop");
+                        }
+                    }else {
+//if the crop is not fully grown then dont replant and dont drop any items
+                        e.getPlayer().sendMessage("The crop is not fully grown yet");
                     }
-                    e.getPlayer().getInventory().addItem(new ItemStack(blockType));
-                    // remove the seed/crop from the player's inventory
-                    int amount = e.getPlayer().getInventory().getItemInMainHand().getAmount();
-                    if (amount > 1) {
-                        e.getPlayer().getInventory().getItemInMainHand().setAmount(amount + 3);
-                    } else {
-                        e.getPlayer().getInventory().setItemInMainHand(null);
-                    }
+                }else {
+//if the block is not a crop block then dont replant and dont drop any items
+                    e.getPlayer().sendMessage("This is not a crop block");
                 }
             }
-        }
-
         }
     }
 
